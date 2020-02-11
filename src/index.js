b8c186f39f36cbc07ffb9c3dcee0b51eade858f9
Rohit Kumar <Rohit.Kumar@pharmacyregulation.org>
2020-02-11 17:06:51 +0000

    first components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
 
 function Hi() {
   return <p>Hi.</p>;
 }
 
-render(<Hi />, document.getElementById("app"));
+render(
+  <Router>
+    <Hi />
+  </Router>,
+  document.getElementById("app")
+);
