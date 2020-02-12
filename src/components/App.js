d072eac26751a9b150b4189dbc82af5fad3df6c1
Rohit Kumar <Rohit.Kumar@pharmacyregulation.org>
2020-02-12 17:15:54 +0000

    some more work

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import {Route} from 'react-router-dom';
-import AboutPage from './about/About';
-import HomePage from './home/HomePage';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import AboutPage from "./about/About";
+import HomePage from "./home/HomePage";
+import Header from "./common/Header";
+import PageNotFound from "./PageNotFound";
+import CoursePage from "./courses/CoursesPage";
 
-function App()  {
-    return (
-        <div className="container-fluid">
-            <Route exact path="/" component={HomePage}/>
-            <Route exact path="/about" component={AboutPage}/>
-        </div>
-    );
+function App() {
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/courses" component={CoursePage} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
